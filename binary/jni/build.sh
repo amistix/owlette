5261c015af15b293f5838e1620ca0711373ec666
amistix <amistixx@gmail.com>
2025-09-16 14:44:50 +0000

    Build miniupnp added

--- binary/jni/build.sh
+++ binary/jni/build.sh
@@ -1,2 +1,10 @@
+if [ -z "$ANDROID_NDK_HOME" -a "$ANDROID_NDK_HOME" == "" ]; then
+	echo -e "\033[31mFailed! ANDROID_NDK_HOME is empty'\033[0m"
+	exit 1
+fi
+
 echo "Building boost..."
 sh build_boost.sh 
+
+echo "Building miniupnpc..."
+sh build_miniupnp.sh
