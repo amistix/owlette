1b8ad7d61db8131de0cc96d58b197e1abf78b16d
amistix <amistixx@gmail.com>
2025-11-05 13:03:40 +0000

    OpenGL ES in java added

--- app/src/main/java/org/amistix/owlette/MainActivity.java
+++ app/src/main/java/org/amistix/owlette/MainActivity.java
@@ -1,20 +1,34 @@
 package org.amistix.owlette;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
+import android.opengl.GLSurfaceView;
 
 import org.amistix.owlette.i2pd.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private DaemonWrapper daemon;
+    private GLSurfaceView glView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        glView = new GLSurfaceView(this);
+        glView.setEGLContextClientVersion(2);
+        glView.setRenderer(new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(javax.microedition.khronos.opengles.GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {}
+
+            @Override
+            public void onSurfaceChanged(javax.microedition.khronos.opengles.GL10 gl, int width, int height) {}
+
+            @Override
+            public void onDrawFrame(javax.microedition.khronos.opengles.GL10 gl) {}
+        });
+
+        setContentView(glView);
 
 
         daemon = DaemonWrapper.getInstance(this);
